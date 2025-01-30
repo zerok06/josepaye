29e0cac1ab9611ef43980c785d998f31ee9a1364
zerok06 <jose.geeksjose@gmail.com>
2025-01-30 13:52:09 -0500

    change: se deshabilito temporalmente el modo dark

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://josepaye.vercel.app',
   integrations: [tailwind(
     {
       applyBaseStyles: false
     }
-  ), react(), mdx()]
+  ), react(), mdx(), sitemap()],
 });

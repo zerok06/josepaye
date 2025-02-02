545289af2c47b0d0be576b6aff411655827481d6
zerok06 <jose.geeksjose@gmail.com>
2025-02-02 15:10:20 -0500

    change: cambio de robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
   ), react(), mdx(), sitemap(
     {
       lastmod: new Date(),
+      entryLimit: 50000,
     }
   )],
 });
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,7 +3,7 @@ import type { APIRoute } from 'astro';
 const robotsTxt = `
 User-agent: *
 Allow: /
-Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
+sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim();
 
 export const GET: APIRoute = () => {
